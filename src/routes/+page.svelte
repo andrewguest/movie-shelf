90bbebb64ab86b73a9d0f97e6f89567173e87854
Andrew Guest <andrew@aguest.me>
2023-04-05 22:02:22 -0500

    add movie cards to grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	// functions
+	async function loadData() {
+		const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+		const json = await response.json();
+
+		return json;
+	}
+
+	onMount(async () => {
+		dataPromise = await loadData();
+	});
+
+	// variables
+	let dataPromise = loadData();
+</script>
+
+<div class="m-4">
+	{#await dataPromise}
+		<p>Loading...</p>
+	{:then data}
+		<ul>
+			{#each Object.keys(data) as key}
+				<li>{key}: {data[key]}</li>
+			{/each}
+		</ul>
+	{:catch error}
+		<p>Error: {error.message}</p>
+	{/await}
+
+	<button
+		on:click={loadData}
+		type="button"
+		class="inline-flex justify-center items-center space-x-2 border font-semibold rounded-lg px-4 py-2 leading-6 border-blue-700 bg-blue-700 text-white hover:text-white hover:bg-blue-600 hover:border-blue-600 focus:ring focus:ring-blue-400 focus:ring-opacity-50 active:bg-blue-700 active:border-blue-700 dark:focus:ring-blue-400 dark:focus:ring-opacity-90"
+		>Load Data</button
+	>
+</div>
---
+++ src/routes/movies/genre_badge.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let genre;
+
+	// assign specific text and background colors to each genre so that the badges are different colors
+	const genre_lookup = {
+		adventure: 'text-emerald-700 bg-emerald-200',
+		fantasy: 'text-teal-700 bg-teal-200',
+		animation: 'text-blue-700 bg-blue-200',
+		drama: 'text-purple-700 bg-purple-200',
+		horror: 'text-pink-700 bg-pink-200',
+		action: 'text-teal-700 bg-teal-200',
+		comedy: 'text-red-700 bg-red-200',
+		history: 'text-orange-700 bg-orange-200',
+		western: 'text-blue-700 bg-blue-200',
+		thriller: 'text-emerald-700 bg-emerald-200',
+		crime: 'text-indigo-700 bg-indigo-200',
+		documentary: 'text-purple-700 bg-purple-200',
+		'science fiction': 'text-pink-700 bg-pink-200',
+		mystery: 'text-rose-700 bg-rose-200',
+		music: 'text-orange-700 bg-orange-200',
+		romance: 'text-purple-700 bg-purple-200',
+		family: 'text-orange-700 bg-orange-200',
+		war: 'text-orange-700 bg-orange-200',
+		'tv movie': 'text-emerald-700 bg-emerald-200'
+	};
+
+	let badge_color = genre_lookup[genre];
+</script>
+
+<h4
+	class={`font-semibold inline-flex px-2 py-1 mx-1 my-1 leading-4 text-sm rounded-md ${badge_color}`}
+>
+	{genre}
+</h4>
