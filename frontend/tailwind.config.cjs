1fc17221a55aa4a220b6f51fb5ed9bfedd1d643b
Andrew Guest <andrew@aguest.me>
2023-04-17 20:58:01 -0500

    Added button that downloads the movie CSV template file

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'space-blue-100': '#3868AA',
+        'space-blue-200': '#224370',
+        'space-blue-300': '#1B3559',
+        'space-gray': '#F0F0F0',
+        'space-yellow': '#F9D949',
+        'space-orange': '#F45050'
+    }
+    },
   },
   plugins: [],
   darkMode: false,
